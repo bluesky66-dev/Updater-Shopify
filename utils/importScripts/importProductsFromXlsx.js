631d629f72de405bd3a62f1018fb909f69511427
Blue Sky <alister11david@gmail.com>
2021-06-03 01:34:29 +0800

    Adding the MAN BAGS

--- utils/importScripts/importProductsFromXlsx.js
+++ utils/importScripts/importProductsFromXlsx.js
@@ -71,14 +71,14 @@ const postProducts = async (storeURL, auth, productsSource, i) => {
 const getProducts = async (storeURL, auth) => {
     console.log('===Reading Products===');
     var workbook = XLSX.readFile('Products/MIXED_ACCESSORIES.xlsx');
-    var first_sheet_name = workbook.SheetNames[2];
+    var first_sheet_name = workbook.SheetNames[3];
     var worksheet = workbook.Sheets[first_sheet_name];
     const products = [];
     let preTitle = '';
     let preCat = '';
     let preHtmlBody = '';
     let product = {};
-    for (let i = 2; i <= 133; i++) {
+    for (let i = 2; i <= 21; i++) {
         console.log(`===Reading Products === ${i}`);
         const category = worksheet[`A${i}`]?.v.trim();
         const title = worksheet[`L${i}`]?.v;
@@ -225,8 +225,8 @@ const getProductsImages = async (worksheet, i) => {
 
     for (let m = 0; m < images.length; m++){
         try {
-            const imagePath1 = `MENS UNDERWEAR/${images[m]}.jpg`;
-            const imagePath2 = `MENS UNDERWEAR/${images[m]}.png`;
+            const imagePath1 = `MAN BAGS/${images[m]}.jpg`;
+            const imagePath2 = `MAN BAGS/${images[m]}.png`;
 
             if (fs.existsSync(`Products/${imagePath1}`)) {
                 // const attachment = fs.readFileSync(imagePath1, {encoding: 'base64'});
